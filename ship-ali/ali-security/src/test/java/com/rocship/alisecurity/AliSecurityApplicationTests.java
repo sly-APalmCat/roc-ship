6f219db5b788ff4cc6b289b216ac3ec67482dde9
APalmCat <zl15303892011@qq.com>
2020-12-04 17:28:06 +0800

    前期

---
+++ ship-ali/ali-security/src/test/java/com/rocship/alisecurity/AliSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocship.alisecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
