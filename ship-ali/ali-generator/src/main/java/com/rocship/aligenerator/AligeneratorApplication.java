03eec9bffbae7c2bae17d726344baf59af2d8133
APalmCat <zl15303892011@qq.com>
2021-01-05 18:07:39 +0800

    en,刚开始，不知道说啥

---
+++ ship-ali/ali-generator/src/main/java/com/rocship/aligenerator/AligeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.rocship.aligenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AligeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AligeneratorApplication.class, args);
+    }
+
+}
