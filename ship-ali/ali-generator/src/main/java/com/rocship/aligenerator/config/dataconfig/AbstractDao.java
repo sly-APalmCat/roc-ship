5add8b4154d022f8106dde603346fe67a9daea6e
APalmCat <zl15303892011@qq.com>
2021-01-06 09:32:24 +0800

    tijiao

--- ship-ali/ali-generator/src/main/java/com/rocship/aligenerator/config/dataconfig/AbstractDao.java
+++ ship-ali/ali-generator/src/main/java/com/rocship/aligenerator/config/dataconfig/AbstractDao.java
@@ -5,17 +5,21 @@ package com.rocship.aligenerator.config.dataconfig;/**
  * @version
  */
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * ClassName: AbstractDao <br/>
  * Description: <br/>
  * date: 2021/1/5 17:59<br/>
  * @author 15438<br />
  */
-public class AbstractDao {
+public interface AbstractDao {
 
+    List<Map<String, Object>> checkoutList(Map<String, Object> map);
 
+    Map<String, String> checkoutTable(String tableName);
 
-
-
+    List<Map<String, String>> checkoutColumns(String tableName);
 
 }
---
+++ ship-ali/ali-generator/src/main/java/com/rocship/aligenerator/dao/MysqlDao.java
@@ -0,0 +1,21 @@
+package com.rocship.aligenerator.dao;/**
+ * Description: <br/>
+ * date: 2021/1/6 9:06<br/>
+ *
+ * @version
+ */
+
+import com.rocship.aligenerator.config.dataconfig.AbstractDao;
+
+
+/**
+ * ClassName: MysqlDao <br/>
+ * Description: <br/>
+ * date: 2021/1/6 9:06<br/>
+ * @author 15438<br />
+ */
+public interface MysqlDao extends AbstractDao {
+
+
+
+}
