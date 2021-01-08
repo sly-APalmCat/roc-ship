b7caf3990f15bfb99a163b7fdefdb00aa7dbbafe
APalmCat <zl15303892011@qq.com>
2021-01-08 11:20:04 +0800

    clear

---
+++ ship-ali/ali-generator/src/main/java/com/rocship/aligenerator/controller/AbTest.java
@@ -0,0 +1,15 @@
+package com.rocship.aligenerator.controller;/**
+ * Description: <br/>
+ * date: 2021/1/8 10:53<br/>
+ *
+ * @version
+ */
+
+/**
+ * ClassName: AbTest <br/>
+ * Description: <br/>
+ * date: 2021/1/8 10:53<br/>
+ * @author 15438<br />
+ */
+public class AbTest {
+}
